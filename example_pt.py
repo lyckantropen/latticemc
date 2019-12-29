75c0baeebdf183987461cad3f39d867174171428
Karol Trojanowski <karol.trojanowski@displaylink.com>
2019-12-29 13:01:52 +0100

    Use local parameters history in parallel example

--- example_pt.py
+++ example_pt.py
@@ -14,27 +14,30 @@ for s in states:
     initializePartiallyOrdered(s.lattice, x=randomQuaternion(1))
 
 
-orderParametersCalculator = OrderParametersCalculator(orderParametersHistory, howOften=1, sinceWhen=1, printEvery=50)
-fluctuationsCalculator = FluctuationsCalculator(orderParametersHistory, window=100, howOften=50, sinceWhen=100, printEvery=50)
-perStateUpdaters = [
-    orderParametersCalculator,
-    fluctuationsCalculator,
-    DerivativeWiggleRateAdjustor(howMany=100, howOften=10, sinceWhen=101),
-    RandomWiggleRateAdjustor(scale=0.001, howOften=10, sinceWhen=1),
-    RandomWiggleRateAdjustor(scale=1.0, resetValue=1.0, howOften=1000, sinceWhen=1000),
-]
-
-
 def doState(state):
     try:
-        for it in range(10000):
+        localHistory = {state.parameters: OrderParametersHistory()}
+        orderParametersCalculator = OrderParametersCalculator(localHistory, howOften=1, sinceWhen=1, printEvery=50)
+        fluctuationsCalculator = FluctuationsCalculator(localHistory, window=100, howOften=50, sinceWhen=100, printEvery=50)
+        perStateUpdaters = [
+            orderParametersCalculator,
+            fluctuationsCalculator,
+            DerivativeWiggleRateAdjustor(howMany=100, howOften=10, sinceWhen=101),
+            RandomWiggleRateAdjustor(scale=0.001, howOften=10, sinceWhen=1),
+            RandomWiggleRateAdjustor(scale=1.0, resetValue=1.0, howOften=1000, sinceWhen=1000),
+        ]
+        for it in range(1000):
             simulationNumba.doLatticeStateUpdate(state)
             for u in perStateUpdaters:
                 u.perform(state)
     except Exception as e:
         failsafeSaveSimulation(e, state, orderParametersHistory)
-    return True
+    return localHistory
 
 
-with Parallel(n_jobs=len(states)) as parallel:
+with Parallel(n_jobs=len(states), backend="loky") as parallel:
     results = parallel(delayed(doState)(state) for state in states)
+    for r in results:
+        p = list(r.keys())[0]
+        orderParametersHistory[p].orderParameters = np.append(orderParametersHistory[p].orderParameters, r[p].orderParameters)
+        orderParametersHistory[p].fluctuations = np.append(orderParametersHistory[p].fluctuations, r[p].fluctuations)
