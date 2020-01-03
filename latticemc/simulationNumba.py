05049c5c8ea3d39c2719a3daeccd7ab1d49ecfe0
Karol Trojanowski <trojanowski.ifuj@gmail.com>
2020-01-03 12:27:08 +0100

    Split the order parameters from state variables in particle dtype

--- latticemc/simulationNumba.py
+++ latticemc/simulationNumba.py
@@ -5,7 +5,7 @@ Monte Carlo lattice simulation accelerated using Numba
 import numpy as np
 from numba import jit, njit
 from .randomQuaternion import wiggleQuaternion
-from .definitions import particle, LatticeState
+from .definitions import LatticeState, particleDoF, particleProps
 from .tensorTools import dot6, dot10, T20AndT22In6Coordinates, T32In10Coordinates, quaternionToOrientation, SQRT2
 from .orderParameters import biaxialOrdering
 
@@ -87,9 +87,10 @@ def _doOrientationSweep(lattice, indexes, temperature, lam, tau, wiggleRate):
     updated using a random walk with radius 'wiggleRate'.
     """
     for _i in indexes:
-        particle = lattice[tuple(_i)]
-        nx = _getNeighbors(_i, lattice['x'])
-        npi = _getNeighbors(_i, lattice['p'][..., np.newaxis])
+        particle = lattice.particles[tuple(_i)]
+        props = lattice.properties[tuple(_i)]
+        nx = _getNeighbors(_i, lattice.particles['x'])
+        npi = _getNeighbors(_i, lattice.particles['p'][..., np.newaxis])
         energy1 = _getEnergy(particle['x'], particle['p'], nx, npi, lam=lam, tau=tau)
 
         # adjust x
@@ -97,7 +98,7 @@ def _doOrientationSweep(lattice, indexes, temperature, lam, tau, wiggleRate):
         energy2 = _getEnergy(x_, particle['p'], nx, npi, lam=lam, tau=tau)
         if _metropolis(2 * (energy2 - energy1), temperature):
             particle['x'] = x_
-            particle['energy'] = energy2
+            props['energy'] = energy2
 
         # adjust p
         if tau != 0:
@@ -105,10 +106,10 @@ def _doOrientationSweep(lattice, indexes, temperature, lam, tau, wiggleRate):
             energy2 = _getEnergy(particle['x'], p_, nx, npi, lam=lam, tau=tau)
             if _metropolis(2 * (energy2 - energy1), temperature):
                 particle['p'] = p_
-                particle['energy'] = energy2
+                props['energy'] = energy2
 
         # instead of using the same t20 and t22 tensors, calculate depending on lambda parameter
-        a, b, c, particle['t32'] = _getPropertiesFromOrientation(particle['x'], particle['p'])
+        a, b, c, props['t32'] = _getPropertiesFromOrientation(particle['x'], particle['p'])
         o = biaxialOrdering(lam)
         if o == 1:
             ex, ey, ez = a, b, c
@@ -116,7 +117,7 @@ def _doOrientationSweep(lattice, indexes, temperature, lam, tau, wiggleRate):
             ex, ey, ez = c, a, b
         if o == 0:
             ex, ey, ez = b, c, a
-        particle['t20'], particle['t22'] = T20AndT22In6Coordinates(ex, ey, ez)
+        props['t20'], props['t22'] = T20AndT22In6Coordinates(ex, ey, ez)
 
 
 @jit(forceobj=True, nopython=False, cache=True)
@@ -125,13 +126,13 @@ def _getLatticeAverages(lattice):
     Calculate state average of per-particle properties
     as specified in the 'particle' data type.
     """
-    avg = np.zeros(1, dtype=particle)
-    avg['x'] = lattice['x'].mean(axis=(0, 1, 2))
-    avg['t20'] = lattice['t20'].mean(axis=(0, 1, 2))
-    avg['t22'] = lattice['t22'].mean(axis=(0, 1, 2))
-    avg['t32'] = lattice['t32'].mean(axis=(0, 1, 2))
-    avg['p'] = lattice['p'].mean()
-    avg['energy'] = lattice['energy'].mean()
+    avg = np.zeros(1, dtype=particleProps)
+    avg['t20'] = lattice.properties['t20'].mean(axis=(0, 1, 2))
+    avg['t22'] = lattice.properties['t22'].mean(axis=(0, 1, 2))
+    avg['t32'] = lattice.properties['t32'].mean(axis=(0, 1, 2))
+    # parity is calculated from the DoF, but needs to be promoted to float
+    avg['p'] = lattice.particles['p'].astype(np.float32).mean()
+    avg['energy'] = lattice.properties['energy'].mean()
     return avg
 
 
@@ -144,15 +145,15 @@ def doLatticeStateUpdate(state: LatticeState):
     state averages of per-particle properties.
     """
     # update particles at random
-    indexes = (state.lattice.particles['index']
+    indexes = (state.lattice.properties['index']
                .reshape(-1, 3)[np.random.randint(0,
                                                  state.lattice.particles.size,
                                                  state.lattice.particles.size)])
-    _doOrientationSweep(state.lattice.particles,
+    _doOrientationSweep(state.lattice,
                         indexes,
                         float(state.parameters.temperature),
                         float(state.parameters.lam),
                         float(state.parameters.tau),
                         state.wiggleRate)
     state.iterations += 1
-    state.latticeAverages = _getLatticeAverages(state.lattice.particles)
+    state.latticeAverages = _getLatticeAverages(state.lattice)
