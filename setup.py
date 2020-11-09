b10ba8a9f9565491ff274851325f94fb6e9363cd
Karol Trojanowski <trojanowski.ifuj@gmail.com>
2020-11-09 22:08:02 +0100

    Refactor setup.py and requirements.txt

--- setup.py
+++ setup.py
@@ -1,18 +1,5 @@
-from setuptools import setup
-
-
-def readlines(file):
-    with open(file) as f:
-        return f.readlines()
-
+from setuptools import find_namespace_packages, setup
 
 setup(
-    name='latticemc',
-    version='0.1',
-    author='Karol Trojanowski',
-    author_email='trojanowski.ifuj@gmail.com',
-    license='MIT',
-    keywords='lattice monte carlo biaxial nematic chirality',
-    packages=['latticemc'],
-    install_requires=readlines('requirements.txt')
+    packages=find_namespace_packages()
 )
